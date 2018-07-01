4c26bdf6a74ac19cba2e65e02dee47861de3e3c3
dengtao <dtqwe@qq.com>
2018-07-01 22:36:40 +0800

    init config

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@
 const router = require('koa-router')({
     prefix: '/weapp'
 })
+// xxx.com/weapp/demo
 const controllers = require('../controllers')
 
 // 从 sdk 中取出中间件
@@ -32,4 +33,5 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+router.get('./demo', controllers.demo)
 module.exports = router
