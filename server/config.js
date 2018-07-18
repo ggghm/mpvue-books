1dd30787a2f2627cfa8c612d5df7c6f980dfb07b
dengtao <dtqwe@qq.com>
2018-07-18 12:45:16 +0800

    gif

--- server/config.js
+++ server/config.js
@@ -1,14 +1,14 @@
 const CONF = {
     // 本地开发的一些设置
-    serverHost: 'localhost',
-    tunnelServerUrl: '',
-    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
-    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
-    qcloudAppId: '1256977323',
-    qcloudSecretId: 'AKIDJWXO3Ze31pRxW09YXLwaHxXFpDPOHk7H',
-    qcloudSecretKey: '60vjNo24wb3MdjcIm37yc3HJ8nGXbS6X',
-    wxMessageToken: 'weixinmsgtoken',
-    networkTimeout: 30000,
+    // serverHost: 'localhost',
+    // tunnelServerUrl: '',
+    // tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+    // // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
+    // qcloudAppId: '1256977323',
+    // qcloudSecretId: 'AKIDJWXO3Ze31pRxW09YXLwaHxXFpDPOHk7H',
+    // qcloudSecretKey: '60vjNo24wb3MdjcIm37yc3HJ8nGXbS6X',
+    // wxMessageToken: 'weixinmsgtoken',
+    // networkTimeout: 30000,
 
     port: '5757',
     rootPathname: '',
@@ -32,7 +32,8 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: '050705',
+        pass: 'wx66525979d3baf553',
+        // pass: '050705',
         char: 'utf8mb4'
     },
 
